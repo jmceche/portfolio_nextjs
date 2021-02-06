8182e16fad1f805adc65fe13a777300619ffe099
juan Martin Cechetto <jmcechetto@outlook.com>
2021-02-06 11:42:17 -0300

    fixed sidebar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,11 +2,12 @@ import { Flex, Link, Icon, Button, Box, Text } from "@chakra-ui/react";
 import { FaRegEnvelope, FaGithub, FaLinkedin, FaBars } from "react-icons/fa";
 import { useState } from "react";
 import { Link as RouteLink } from "next";
+import SideBar from "./Sidebar";
+import NavLinks from "./NavLinks";
 
 const Navbar = ({ act }) => {
   const [show, setShow] = useState(true);
   const [copy, setCopy] = useState("Copy");
-  const [displayNav, setDisplayNav] = useState("none");
 
   const handleCopy = () => {
     copyMail();
@@ -14,10 +15,6 @@ const Navbar = ({ act }) => {
     const t = setTimeout(() => {
       setCopy("Copy");
     }, 4000);
-  };
-
-  const handleHide = () => {
-    displayNav === "none" ? setDisplayNav("block") : setDisplayNav("none");
   };
 
   const copyMail = async () => {
@@ -30,12 +27,12 @@ const Navbar = ({ act }) => {
 
   return (
     <Flex
-      w={["auto", "100vw"]}
-      h={["auto", "5rem"]}
+      w='100vw'
+      h='5rem'
       bg='#f8f9fa'
       as='nav'
-      direction={["column", "row"]}
-      align={["flex-start", "center"]}
+      direction='row'
+      align='center'
       grow
       position='fixed'
       top='0'
@@ -43,83 +40,25 @@ const Navbar = ({ act }) => {
       zIndex='10'
       p='.6rem'
     >
-      <Button
-        variant='unstyled'
-        display={["block", "none"]}
-        onClick={handleHide}
-        alignSelf='flex-start'
-      >
-        <Icon as={FaBars} w={8} h={8}></Icon>
-      </Button>
-      <Flex
-        w={["70vw", "100%"]}
-        h={["100vh", "auto"]}
-        display={[displayNav, "flex"]}
-        direction={["column", "row"]}
-        justify={["space-around", "space-between"]}
-        align='flex-start'
-      >
+      <SideBar />
+      <Flex w='100%' direction='row' justify='space-between' align='center'>
         <Flex
           w='100%'
-          align='flex-start'
-          justify={["space-around", "flex-start"]}
-          direction={["column", "row"]}
+          align='center'
+          justify='flex-start'
+          direction='row'
+          display={["none", "flex"]}
         >
-          <Link
-            my={[".5rem", "auto"]}
-            ml={[0, "2rem"]}
-            style={{ fontWeight: act[0] ? "bold" : 400 }}
-            as={RouteLink}
-            href='#home'
-          >
-            Home
-          </Link>
-          <Link
-            my={[".5rem", "auto"]}
-            ml={[0, "2rem"]}
-            as={RouteLink}
-            href='#projects'
-            style={{ fontWeight: act[1] ? "bold" : 400 }}
-          >
-            Projects
-          </Link>
-          <Link
-            my={[".5rem", "auto"]}
-            ml={[0, "2rem"]}
-            as={RouteLink}
-            href='#about'
-            style={{ fontWeight: act[2] ? "bold" : 400 }}
-          >
-            About Me
-          </Link>
-          <Link
-            href='https://drive.google.com/file/d/1MGkA2z1SLepjdKL7qB9lRnbm6Ah3FyEf/view?usp=sharing'
-            target='_blank'
-            my={[".5rem", "auto"]}
-            ml={[0, "2rem"]}
-          >
-            CV
-          </Link>
+          <NavLinks ml='2rem' />
         </Flex>
-        <Flex
-          w='100%'
-          direction={["column", "row"]}
-          justify={["flex-start", "flex-end"]}
-        >
+        <Flex w='100%' direction='row' justify='flex-end'>
           <Box
             position='relative'
             onMouseEnter={() => setShow(false)}
             onMouseLeave={() => setShow(true)}
           >
             <Icon as={FaRegEnvelope} w={8} h={8} mr='1rem'></Icon>
-            <Link
-              display={["inline-block", "none"]}
-              href='mailto: jmcechetto@gmail.com'
-            >
-              Email Me
-            </Link>
             <Box
-              display={["none", "block"]}
               position='absolute'
               top='120%'
               right='-150%'
@@ -151,9 +90,6 @@ const Navbar = ({ act }) => {
           </Box>
           <Link href={"https://github.com/jmceche"} target='_blank' mr='1rem'>
             <Icon as={FaGithub} w={8} h={8}></Icon>
-            <Text ml='1rem' display={["inline-block", "none"]}>
-              Github
-            </Text>
           </Link>
           <Link
             href={
@@ -163,9 +99,6 @@ const Navbar = ({ act }) => {
             mr='1rem'
           >
             <Icon as={FaLinkedin} w={8} h={8}></Icon>
-            <Text ml='1rem' display={["inline-block", "none"]}>
-              Linkedin
-            </Text>
           </Link>
         </Flex>
       </Flex>
